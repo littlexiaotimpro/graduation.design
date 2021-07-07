5b29adb82d7ad198afeb8d246d9cfd01b34c4323
littlexiaotimpro <a99996655@163.com>
2021-07-07 16:20:49 +0800

    重构项目

--- blog/src/main/java/com/whoai/blog/config/interceptor/ErrorInterceptor.java
+++ blog/src/main/java/com/whoai/blog/config/interceptor/ErrorInterceptor.java
@@ -16,7 +16,7 @@ public class ErrorInterceptor implements HandlerInterceptor {
     @Override
     @SuppressWarnings("NullableProblems")
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        logger.info(">>>>>>>在请求处理之前进行调用（Controller方法调用之前）");
+        logger.info("在请求处理之前进行调用（Controller方法调用之前）");
         String cookies = JWTUtil.parseCookies(request);
         return cookies != null;// 只有返回true才会继续向下执行，返回false取消当前请求
     }
@@ -27,7 +27,7 @@ public class ErrorInterceptor implements HandlerInterceptor {
     @Override
     @SuppressWarnings("NullableProblems")
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        logger.info(">>>>>>请求处理之后进行调用，但是在视图被渲染之前（Controller方法调用之后）");
+        logger.info("请求处理之后进行调用，但是在视图被渲染之前（Controller方法调用之后）");
         if (response.getStatus() == 404) {
             // TODO 无法获取资源时自动跳转至404页面
             //response.sendRedirect("/404");
@@ -37,6 +37,6 @@ public class ErrorInterceptor implements HandlerInterceptor {
     @Override
     @SuppressWarnings("NullableProblems")
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        logger.info(">>>>>>>在整个请求结束之后被调用，也就是在DispatcherServlet 渲染了对应的视图之后执行（主要是用于进行资源清理工作）");
+        logger.info("在整个请求结束之后被调用，也就是在DispatcherServlet 渲染了对应的视图之后执行（主要是用于进行资源清理工作）");
     }
 }
